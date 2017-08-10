d56d6a964cd7d34a0fbffbd2444320d653f95cb2
Marek Gerhart <marek.gerhart@gooddata.com>
2017-08-10 21:57:39 +0200

    Initial - bootstrap commit
    Just some folder structure + setting up gradle

---
+++ README.md
@@ -0,0 +1,9 @@
+# Zonky app
+## Running the app
+* gradle run
+
+## Running test
+* gradle test
+
+## Bundling the app
+* TODO
---
+++ src/main/java/com/mawek/zzz/MainApp.java
@@ -0,0 +1,8 @@
+package com.mawek.zzz;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
